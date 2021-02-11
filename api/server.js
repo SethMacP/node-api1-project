b9c5a89cea8300bbc1695cc1d72bcccc93c13659
SethMacP <macpherson.seth@gmail.com>
2021-02-10 16:02:54 -0800

    all crud commands setup, project initialized

--- api/server.js
+++ api/server.js
@@ -1,3 +1,24 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const model = require('./users/model');
+const server = express();
 
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+    
+})
+server.post("/api/users", (req,res)=>{
+
+})
+server.get("/api/users/:id", (req,res)=>{
+
+})
+server.delete("/api/users/:id",(req,res)=>{
+
+})
+server.put("/api/users/:id",(req,res)=>{
+    
+})
+//
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
